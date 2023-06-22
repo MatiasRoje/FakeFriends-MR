db7bf30d787c4a5daebe767603566c2d87aa9c80
Matias Roje <122602502+MatiasRoje@users.noreply.github.com>
2023-06-22 11:11:35 +0200

    Revert "Revert "Refactoring backend""

--- app/controllers/room_questions_controller.rb
+++ app/controllers/room_questions_controller.rb
@@ -3,29 +3,42 @@ class RoomQuestionsController < ApplicationController
     @users = User.all
     @room = Room.find(params[:room_id])
     @room_question = RoomQuestion.find(params[:id])
+
+    # Data to be transfered to the change_page Stimulus controller
     @room_questions = @room_question.room.room_questions
-    @array_of_urls = []
-    @room_questions.each do |question|
-      @array_of_urls << room_room_question_path(question.room, question)
-    end
+    @array_of_urls = @room_questions.map { |question| questions = room_room_question_path(question.room, question) }
 
     if @room_question.round == 1
       @answer = Answer.new
     end
 
     if @room_question.round == 2
+      # There is only one answer stored in the room_question and is
+      # the answer of a random user of the same question on the first round
       @right_answer = Answer.where(room_question_id: @room_question)
-      @user_answers = UserAnswer.where(room_id: @room)
-      @room_users = RoomUser.where(room_id: @room)
-      @right_collection = []
-        @room_users.each do |user|
-          if user.user != current_user
-            @right_collection << user.user
+
+      # All users answers on the room for that question, this need is for
+      # creating the simple form with users as answers for the second round
+      @user_as_answers = []
+      RoomUser.where(room_id: @room).each do |room_user|
+        # The user shouldn't see himself appearing on the form
+        if room_user.user != current_user
+          @user_as_answers << room_user.user
+        end
+      end
+
+      # Data to be transfered to the add_username Stimulus controller
+      @user_as_answers_usernames = @user_as_answers.map { |user| user.username }
+
+      # Data for showing the users who answered the @right_answer
+      @users_with_right_answer = []
+      RoomUser.where(room_id: @room).each do |room_user|
+        user_answers_in_room = UserAnswer.where(room_id: @room, user_id: room_user.user.id)
+        user_answers_in_room.each do |answer|
+          if answer.answer.content == @right_answer.first.content
+            @users_with_right_answer << room_user.user
           end
         end
-      @array_of_usernames = []
-      @right_collection.each do |user|
-        @array_of_usernames << user.username
       end
     end
   end
@@ -48,24 +61,27 @@ class RoomQuestionsController < ApplicationController
 
     if @room_question.round == 2
       @right_answer = Answer.where(room_question_id: @room_question)
-      @picked_users = []
-      @array_of_ids = params[:room_question][:answer_ids]
-      @array_of_ids.delete_at(0)
-      @array_of_ids.each do |number|
-        user_from_form = User.find(number)
-        @picked_users << user_from_form
+      @selected_users = []
+
+      # Targeting the user input for the form
+      @user_input_answer_ids = params[:room_question][:answer_ids]
+      @user_input_answer_ids.delete_at(0)
+      @user_input_answer_ids.each do |id|
+        user_from_form = User.find(id)
+        @selected_users << user_from_form
       end
-      @room_user = RoomUser.find_by(room_id: @room, user_id: current_user)
-      @picked_users.each do |u|
-        user_answers_in_room = UserAnswer.where(room_id: @room, user_id: u)
+      @current_user_as_room_user = RoomUser.find_by(room_id: @room, user_id: current_user)
+      @selected_users.each do |user|
+        # Finding all user answers in the room
+        user_answers_in_room = UserAnswer.where(room_id: @room, user_id: user)
         user_answers_in_room.each do |user_answer|
           if user_answer.answer.question == @room_question.question
             if user_answer.answer.content == @right_answer.first.content
-              @room_user.counter += 1
-              @room_user.save
+              @current_user_as_room_user.counter += 1
+              @current_user_as_room_user.save
             else
-              @room_user.counter -= 1
-              @room_user.save
+              @current_user_as_room_user.counter -= 1
+              @current_user_as_room_user.save
             end
           end
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'informations', to: 'pages#informations'
-  get "your_games", to: "pages#your_games", as: "your_games"
-  get "enter_room", to: "pages#enter_room", as: "enter_room"
-  post "post_form", to: "pages#post_form", as: "post_form"
+  get "about", to: "pages#about"
+  get "join_room", to: "pages#join_room", as: "join_room"
+  post "post_join_room", to: "pages#post_join_room", as: "post_join_room"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :user_answers, only: [:create]
-
   resources :rooms, only: %i[index show new create] do
-    resources :room_questions, only: %i[index show new create update]
-    get "creating_round", to: "pages#creating_round", as: "creating_round"
-    post "post_round", to: "pages#post_round", as: "post_round"
-    get "ranking", to: "pages#ranking", as: "ranking_room"
+    resources :room_questions, only: %i[show new create update]
+    get "new_round", to: "rooms#new_round", as: "new_round"
+    post "create_round", to: "rooms#create_round", as: "create_round"
+    get "ranking", to: "rooms#ranking", as: "ranking_room"
   end
 end
